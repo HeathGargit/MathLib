c5715f567f569f4a033b2b08fc0a37fa7a33ae44
Heath Gargit <Heath Gargit>
2016-09-14 22:05:21 +1000

    Initial Local Commit. Added Solution

---
+++ MathApp/MathApp.cpp
@@ -0,0 +1,21 @@
+// MathApp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Utility.h"
+
+#include <iostream>
+
+
+int main()
+{
+	std::cout << add(10, 5) << std::endl;
+	system("pause");
+
+	system("CLS");
+	std::cout << add(10, 6) << std::endl;
+	system("pause");
+
+    return 0;
+}
+
