442a64add733888b8c128d6e9a01c90807126f02
Heath Gargit <Heath Gargit>
2016-09-24 16:38:18 +1000

    Finshed Binary Exercises, inc Bonus Challenge. Also Added Multiplication and Division. I'm feeling pretty good right now.

--- MathLib/Utility.h
+++ MathLib/Utility.h
@@ -2,6 +2,20 @@
 
 int add(int a, int b);
 
+int multiply(unsigned int a, unsigned int b);
+
+int divide(unsigned int a, unsigned int b);
+
 bool isLeftMostBitSet(unsigned int value);
 
 bool isRightMostBitSet(unsigned int value);
+
+bool isBitSet(unsigned int value, unsigned char bit_to_check);
+
+int GetRightMostSetBit(unsigned int value);
+
+int GetLeftMostSetBit(unsigned int value);
+
+void PrintBinary(unsigned char value);
+
+bool isPowerOfTwo(unsigned int value);
